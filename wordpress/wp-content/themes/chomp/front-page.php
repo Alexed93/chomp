bd3c963820e92543dc123a19a181a9f5b63f7fae
Alex Edwards <alexed93@gmail.com>
2017-12-20 17:36:20 +0000

    Ported over front-page html

--- wordpress/wp-content/themes/chomp/front-page.php
+++ wordpress/wp-content/themes/chomp/front-page.php
@@ -6,7 +6,7 @@
  ***************************************************************************
  *
  * This is used as a bespoke template for the homepage
- * 
+ *
  * More info can be found here:
  * http://codex.wordpress.org/Creating_a_Static_Front_Page
  *
@@ -19,10 +19,153 @@ get_header();
 
 ?>
 
-<main class="section">
-    <div class="container">
+<main>
 
-    </div><!-- .container -->
-</main><!-- .section -->
+    <div class="hero hero--home | u-push-top"> <!-- Hero start -->
+        <div class="container container--small"> <!-- Hero container start -->
+
+            <h1 class="u-align-center u-push-bottom/2">Let's go out
+                <span class="u-weight-medium">tonight</span>
+            </h1> <!-- Dynamic depending on time of day e.g. today/tonight -->
+
+            <form class="u-align-center u-push-bottom u-margin-center form form--search" action=""> <!-- Search form start -->
+
+                <label for="search" class="is-hidden">Search for a restaurant</label>
+                <input type="text" name="search" placeholder="What do you fancy?" class="search">
+
+                <!-- Search button start -->
+                <button type="button" class="search-submit">
+                    <span class="icon icon--small icon--search"></span>
+                    <span class="is-hidden">Search</span>
+                </button>
+                <!-- Search button end -->
+
+                <!-- Popular searches examples start -->
+                <div class="popular u-align-left u-push-top/2">Popular searches:
+                    <a href="#" class="u-style-underline u-style-italic | popular__link">Leeds,</a>
+                    <a href="#" class="u-style-underline u-style-italic | popular__link">Italian,</a>
+                    <a href="#" class="u-style-underline u-style-italic | popular__link">Indian</a>...
+                </div> <!-- Popular searches examples end -->
+
+            </form> <!-- Search form end -->
+
+        </div> <!-- Hero container end -->
+    </div> <!-- Hero end -->
+
+    <div class="promo-area u-pad-bottom@2"> <!-- Promoted restaurants start -->
+        <div class="container"> <!-- Promoted restaurants container start -->
+            <div class="featured test--flexbox cf"> <!-- Promoted restaurants flexbox start -->
+
+                <h1 class="beta u-pad-top sub-heading">Top <span class="u-weight-medium">Italian</span> picks for <span class="u-weight-medium">October 2017</span></h1> <!-- Dynamic depending on date and cuisine of the month -->
+
+                <div class="grid grid--flex"> <!-- Promoted restaurants grid start -->
+
+                    <div class="grid__item grid__item--4-12-bp4"> <!-- Featured card grid item start -->
+
+                        <div class="card card--featured"> <!-- Featured card start -->
+
+                            <div class="card__image"> <!-- Featured card image start -->
+                                <img src="assets/dist/imgs/restaurants/Zucco.jpg" alt="Zucco">
+                            </div> <!-- Featured card image end -->
+
+                            <div class="card__detail"> <!-- Featured card detail start -->
+
+                                <h1 class="delta u-weight-medium u-push-bottom/2">Zucco</h1> <!-- Featured card detail title -->
+                                <h2 class="epsilon u-weight-medium u-push-bottom/2">603 Meanwood Road, LS6 4AY</h2> <!-- Featured card address  -->
+                                <p class="u-zero-bottom">The menu changes regularly depending on what ingredients we can source from our suppliers on the day, however some items we are not allowed to change for fear of upsetting our regulars.</p> <!-- Featured card detail intro -->
+
+                                <div class="card__inputs"> <!-- Featured card specific icons start -->
+
+                                    <div class="details u-float-left"> <!-- Featured card specific icons container start -->
+                                        <span class="icon icon--medium icon--Italian"></span>
+                                        <span class="icon icon--medium icon--Family-Friendly"></span>
+                                        <span class="icon icon--medium icon--Open-Late"></span>
+                                        <span class="icon icon--medium icon--Parking"></span>
+                                    </div> <!-- Featured card specific icons container end -->
+
+                                    <a href="#" class="btn btn--primary cf u-float-right"> <!-- Featured card view button start -->
+                                        <span class="btn--label">View</span>
+                                        <span class="btn--icon">
+                                            <span class="icon icon--small icon--chevron-right-white"></span>
+                                        </span>
+                                    </a> <!-- Featured card view button end -->
+
+                                </div> <!-- Featured card specific icons end -->
+
+                            </div> <!-- Featured card detail end -->
+
+                        </div> <!-- Featured card end -->
+
+                    </div> <!-- Featured card grid item end -->
+
+                </div> <!-- Promoted restaurants grid end -->
+
+                <!-- <a href="#" class="delta u-weight-light u-display-inline u-push-top sub-link">View all previous top picks</a> --> -->
+
+                    <div class="voucher u-push-top@2 u-push-bottom u-display-inline"> <!-- Monthly voucher start -->
+                        <img src="assets/dist/imgs/voucher.png" alt="Voucher" class="voucher__img"> <!-- Monthly voucher image start -->
+                        <div class="voucher__text"> <!-- Monthly voucher text start -->
+                            <h3 class="zeta u-push-bottom/2">Deal of the month:</h3> <!-- Monthly voucher title -->
+                            <div class="qr cf">
+                                <h2 class="gamma u-push-bottom/2 u-weight-medium deal">25% off your total bill at Trattoria Il Forno</h2>
+                                <img src="assets/dist/imgs/qr.png" alt="QR Code to scan"> <!-- Monthly voucher QR image -->
+                            </div>
+                        </div> <!-- Monthly voucher text end -->
+                    </div> <!-- Monthly voucher end -->
+
+                <!-- Print monthly voucher button -->
+                <a href="#" class="delta u-weight-light u-display-inline u-push-top sub-link">Print this deal</a>
+
+            </div> <!-- Promoted restaurants flexbox end -->
+        </div> <!-- Promoted restaurants container end -->
+    </div> <!-- Promoted restaurants end -->
+
+    <div class="container additional-restaurants cf"> <!-- Additional restaurants start -->
+        <h1 class="beta u-push-top sub-heading--inverted u-weight-medium">Other popular restaruants in Leeds</h1>
+
+        <div class="results test--flexbox"> <!-- Additional restaurants results start -->
+            <div class="grid grid--flex results--grid"> <!-- Additional restaurants results grid start -->
+
+                <div class="grid__item grid__item--6-12-bp2"> <!-- Additional restaurants results grid item start -->
+
+                   <div class="card card--animated"> <!-- Additional restaurants card start -->
+
+                        <div class="card__detail"> <!-- Additional restaurants card text start -->
+                            <h1 class="delta u-weight-medium u-push-bottom/2">The Man Behind the Curtain</h1>
+                            <h2 class="epsilon u-weight-medium u-push-bottom/2">68-78 Vicar Lane, LS1 7JH</h2>
+                            <p class="u-zero-bottom">Our Permanent collection is served as a set tasting menu of between 10 and 14 courses, for tables of up to 5 people.</p>
+
+                            <div class="card__inputs"> <!-- Additional restaurant card specific icons start -->
+
+                                <div class="details u-float-left"> <!-- Additional restaurant card specific icons container start -->
+                                    <span class="icon icon--medium icon--English"></span>
+                                    <span class="icon icon--medium icon--Parking"></span>
+                                    <span class="icon icon--medium icon--Alcohol"></span>
+                                </div> <!-- Additional restaurant card specific icons container end -->
+
+                                <a href="#" class="btn btn--primary cf u-float-right"> <!-- Additional restaurant card view button start -->
+                                    <span class="btn--label">View</span>
+                                    <span class="btn--icon">
+                                        <span class="icon icon--small icon--chevron-right-white"></span>
+                                    </span>
+                                </a> <!-- Additional restaurant card view button end -->
+
+                            </div> <!-- Additional restaurant card specific icons end -->
+
+                        </div> <!-- Additional restaurant card detail end -->
+
+                    </div> <!-- Additional restaurant card end -->
+
+                </div> <!-- Additional restaurant card grid item end -->
+
+            </div> <!-- Additional restaurants grid end -->
+
+        <a href="#" class="delta u-weight-light u-display-inline u-push-top u-push-bottom@2 sub-link">View more restaurants</a>
+
+        </div> <!-- Additional restaurants results end -->
+
+    </div> <!-- Additional restaurants end -->
+
+</main>
 
 <?php get_footer(); ?>
