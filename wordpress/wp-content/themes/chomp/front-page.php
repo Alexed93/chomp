edcf55fcfafc63f25528a7000fd6dcd2414253cf
Alex Edwards <alexed93@gmail.com>
2017-12-26 13:50:25 +0000

    Added all restaurants, completed homepage loops etc

--- wordpress/wp-content/themes/chomp/front-page.php
+++ wordpress/wp-content/themes/chomp/front-page.php
@@ -126,7 +126,9 @@ $featured_restaurants = get_field('featured_restaurants');
                                 // Build address
                                 $address_line = get_field('restaurant_address_line', $featured_restaurant->ID);
                                 $postcode = get_field('restaurant_postcode', $featured_restaurant->ID);
-                                $address = $address_line." ".$postcode;
+                                if( $address_line && $postcode ):
+                                    $address = $address_line.", ".$postcode;
+                                endif;
                             ?>
 
                     <div class="grid__item grid__item--4-12-bp4"> <!-- Featured card grid item start -->
@@ -250,24 +252,83 @@ $featured_restaurants = get_field('featured_restaurants');
         <div class="results test--flexbox"> <!-- Additional restaurants results start -->
             <div class="grid grid--flex results--grid"> <!-- Additional restaurants results grid start -->
 
+            <?php
+                // Get the IDs of the 3 restuarants marked as "featured"
+                $excludes = get_field('featured_restaurants');
+                if($excludes):
+                    $featured_restaurant = array();
+                    foreach ($excludes as $excluded):
+                        $featured_restaurant[] = $excluded->ID;
+                    endforeach;
+                endif;
+
+                // Get remaining restaurants
+                $restaurants = chomp_get_restaurants(
+                    6,
+                    $orderby = 'menu_order',
+                    $order = 'ASC',
+                    $excludes = $featured_restaurant
+                );
+
+            ?>
+
+            <?php if( $restaurants->have_posts() ): ?>
+                <?php while ( $restaurants->have_posts() ): $restaurants->the_post(); ?>
+                <?php
+                    // Get cuisine
+                    $restaurant_cuisine = get_the_terms( get_the_ID(), 'cuisine' );
+                    $restaurant_cuisine_name = $restaurant_cuisine[0]->name;
+
+                    // Build address line
+                    $address_line = get_field('restaurant_address_line');
+                    $postcode = get_field('restaurant_postcode');
+                    if( $address_line && $postcode ):
+                        $address = $address_line.", ".$postcode;
+                    endif;
+
+                    // Get features
+                    $restaurant_features = get_field('restaurant_features');
+
+                    if(in_array('alcohol', $restaurant_features)):
+                        $feature_icon['alcohol'] = "Alcohol";
+                    endif;
+
+                    if(in_array('family_friendly', $restaurant_features)):
+                        $feature_icon['family_friendly'] = "Family-Friendly";
+                    endif;
+
+                    if(in_array('open_late', $restaurant_features)):
+                        $feature_icon['open_late'] = "Open-Late";
+                    endif;
+
+                    if(in_array('parking', $restaurant_features)):
+                        $feature_icon['parking'] = "Parking";
+                    endif;
+
+                ?>
+
                 <div class="grid__item grid__item--6-12-bp2"> <!-- Additional restaurants results grid item start -->
 
                    <div class="card card--animated"> <!-- Additional restaurants card start -->
 
                         <div class="card__detail"> <!-- Additional restaurants card text start -->
-                            <h1 class="delta u-weight-medium u-push-bottom/2">The Man Behind the Curtain</h1>
-                            <h2 class="epsilon u-weight-medium u-push-bottom/2">68-78 Vicar Lane, LS1 7JH</h2>
-                            <p class="u-zero-bottom">Our Permanent collection is served as a set tasting menu of between 10 and 14 courses, for tables of up to 5 people.</p>
+                            <h1 class="delta u-weight-medium u-push-bottom/2"><?php echo the_title(); ?></h1>
+                            <h2 class="epsilon u-weight-medium u-push-bottom/2"><?php echo $address; ?></h2>
+                            <p class="u-zero-bottom"><?php the_excerpt(); ?></p>
 
                             <div class="card__inputs"> <!-- Additional restaurant card specific icons start -->
 
+
                                 <div class="details u-float-left"> <!-- Additional restaurant card specific icons container start -->
-                                    <span class="icon icon--medium icon--English"></span>
-                                    <span class="icon icon--medium icon--Parking"></span>
-                                    <span class="icon icon--medium icon--Alcohol"></span>
+                                    <span class="icon icon--medium icon--<?php echo $restaurant_cuisine_name; ?>"></span>
+                                    <?php if($restaurant_features): ?>
+                                        <?php foreach($restaurant_features as $restaurant_feature): ?>
+                                            <span class="icon icon--medium icon--<?php echo $feature_icon[$restaurant_feature]; ?>"></span>
+                                        <?php endforeach; ?>
+                                    <?php endif; ?>
                                 </div> <!-- Additional restaurant card specific icons container end -->
 
-                                <a href="#" class="btn btn--primary cf u-float-right"> <!-- Additional restaurant card view button start -->
+                                <a href="<?php echo get_page_link(); ?>" class="btn btn--primary cf u-float-right"> <!-- Additional restaurant card view button start -->
                                     <span class="btn--label">View</span>
                                     <span class="btn--icon">
                                         <span class="icon icon--small icon--chevron-right-white"></span>
@@ -282,9 +343,13 @@ $featured_restaurants = get_field('featured_restaurants');
 
                 </div> <!-- Additional restaurant card grid item end -->
 
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+                <?php endif; ?>
+
             </div> <!-- Additional restaurants grid end -->
 
-        <a href="#" class="delta u-weight-light u-display-inline u-push-top u-push-bottom@2 sub-link">View more restaurants</a>
+        <a href="<?php echo get_page_link(20); ?>" class="delta u-weight-light u-display-inline u-push-top u-push-bottom@2 sub-link">View more restaurants</a>
 
         </div> <!-- Additional restaurants results end -->
 
