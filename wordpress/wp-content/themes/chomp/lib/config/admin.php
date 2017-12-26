134fa2d75ffd4cc45dd4f31138724b20d79d0baf
Alex Edwards <alexed93@gmail.com>
2017-12-26 14:45:47 +0000

    Show admin bar again

--- wordpress/wp-content/themes/chomp/lib/config/admin.php
+++ wordpress/wp-content/themes/chomp/lib/config/admin.php
@@ -25,7 +25,7 @@
  * $. Remove toolbar on front-end (appears when logged in)
  ******************************************************************************/
 
-add_filter( 'show_admin_bar', '__return_false' );
+// add_filter( 'show_admin_bar', '__return_false' );
 
 
 
